b5ec21b38c149e0a7e9ae6994e90b5b99c89cf7e
Carlos Mira <carlosgarciamira@gmail.com>
2022-01-18 16:13:10 +0000

    Removed unnecesary code

--- stem/stem.py
+++ stem/stem.py
@@ -3,21 +3,14 @@ from stem.helpers import *
 from plane.plane import *
 
 class Stem:   
-    def create_stem(filename):
-        xlist = []
-        xyz_plane = []
-        xyz_plane_rotated = []
-        file_name = filename[:-4] 
-        savefilename = file_name + '_plane' + '.pcd'
+    def create_stem(filename, savefilename):
+        xlist = read_lines(filename)
         
         pcd = o3d.io.read_point_cloud(filename)
         
         Stem.floor_a, Stem.floor_b, Stem.floor_c, Stem.floor_d = Plane.get_plane(pcd)
-        
-        xlist = read_lines(filename, xlist)
-        
-        xyz_plane, xyz_plane_rotated = Plane.append_plane_to_pcd(xyz_plane, xyz_plane_rotated, 
-                                                                 Stem.floor_a, Stem.floor_b, Stem.floor_c, Stem.floor_d)
+               
+        xyz_plane, xyz_plane_rotated = Plane.append_plane_to_pcd(Stem.floor_a, Stem.floor_b, Stem.floor_c, Stem.floor_d)
         
         xlist = append_points_to_list(xyz_plane, xlist)
         xlist = append_points_to_list(xyz_plane_rotated, xlist)        
@@ -28,7 +21,5 @@ class Stem:
         xyz_plane_rotated_pcd.points = o3d.utility.Vector3dVector(xyz_plane_rotated)
         
         create_pcd(savefilename, xlist)
-        pcd = o3d.io.read_point_cloud(savefilename)        
-        o3d.visualization.draw_geometries([pcd])
         
         return xyz_plane_rotated_pcd
