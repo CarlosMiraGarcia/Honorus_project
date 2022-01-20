4e59ba0148d481f638b69933455b55ec95bbdfd3
Carlos Mira <carlosgarciamira@gmail.com>
2022-01-20 11:45:47 +0000

    Added comments and print statements to see progress and results

--- run.py
+++ run.py
@@ -10,77 +10,62 @@ def run(filename):
     # Variables
     list_points_nofloor = []
     list_points_leaves = []
-    diff_adj = 5
     # Records the starting time when the script was run
     start = time.time()
     
     # Creates a new name for the file to be saved based on the original name of the file
     savefilename = filename[:-4] + '_plane' + '.pcd'
+    
     # Reads the point cloud from the filename
     pc_raw = o3d.io.read_point_cloud(filename)
        
     # Removes outliers and returns the inliners and index 
     pc_inliners, ind = Point_cloud.remove_outliers(pc_raw)
     pc_cleaned = pc_inliners.select_by_index(ind)
-
-    #o3d.visualization.draw_geometries([pc_raw])
+    list_points = np.asarray(pc_cleaned.points)
 
     # Finds the segment plane equation for the floor
-    floor_a, floor_b, floor_c, floor_d = Plane.get_plane(pc_cleaned)
+    floor_plane_a, floor_plane_b, floor_plane_c, floor_plane_d = Plane.get_plane(pc_cleaned)
 
     # Returns two numpy arrays: floor and stem
-    floor, stem = Stem.create_stem(pc_cleaned, floor_a, floor_b, floor_c, floor_d)
-    
-    # Inserts planes into original point cloud
-    #np_array_cleaned = Point_cloud.read_lines(pc_cleaned)
-    #np_array_cleaned = Point_cloud.append_points_to_array(floor, np_array_cleaned)
-    #np_array_cleaned = Point_cloud.append_points_to_array(stem, np_array_cleaned)  
-    #pc_cleaned = Point_cloud.array_to_point_cloud(np_array_cleaned)
+    floor, stem = Stem.create_stem(pc_cleaned, floor_plane_a, floor_plane_b, floor_plane_c, floor_plane_d)
 
-    #o3d.visualization.draw_geometries([pc_cleaned])
-    
     # Finds the segment plane equation for the stem
     stem_pc = Point_cloud.array_to_point_cloud(stem)
-    stem_a, stem_b, stem_c, stem_d = Plane.get_plane(stem_pc)
+    stem_plane_a, stem_plane_b, stem_plane_c, stem_plane_d = Plane.get_plane(stem_pc)
     
     # Calculate angles
-    Angle.calculate_angles(floor_a, floor_b, floor_c, stem_a, stem_b, stem_c)
+    print("")
+    print("\033[4mCalculating angle\033[0m")
+    Angle.calculate_angles(floor_plane_a, floor_plane_b, floor_plane_c, stem_plane_a, stem_plane_b, stem_plane_c)
     
     # Removes everything under floor plane
-    list_points = np.asarray(pc_cleaned.points)    
+    print("\033[4mRemoving floor from point cloud\033[0m")
+    list_points_nofloor = Point_cloud.crop_using_plane(list_points, floor_plane_a, floor_plane_b, floor_plane_c, floor_plane_d)
+    array_points_nofloor = np.asarray(list_points_nofloor)
+    point_cloud_nofloor = Point_cloud.array_to_point_cloud(array_points_nofloor) 
     print("Initial array size: ", list_points.size)
+    print("Array size after cropping: ", array_points_nofloor.size)
+    print("")
     
-    for line in list_points:
-        if (floor_a * line[0]) + (floor_b * line[1]) + (floor_c * line[2]) > - floor_d + diff_adj:
-            list_points_nofloor.append(line)    
+    # Removes everything under crops' tray
+    print("\033[4mRemoving tray from point cloud\033[0m")
+    tray_plane_a, tray_plane_b, tray_plane_c, tray_plane_d = Plane.get_plane(point_cloud_nofloor)
+    list_points_leaves = Point_cloud.crop_using_plane(array_points_nofloor, tray_plane_a, tray_plane_b, tray_plane_c, tray_plane_d)
+    array_points_leaves = np.asarray(list_points_leaves)    
+    point_cloud_leaves = Point_cloud.array_to_point_cloud(array_points_leaves)    
+    print("Initial array size: ", array_points_nofloor.size)
+    print("Array size after cropping: ", array_points_leaves.size)
     
-    array_points_kept = np.asarray(list_points_nofloor)
-    print("Array size after cropping: ", array_points_kept.size)
-    point_cloud_nofloor = Point_cloud.array_to_point_cloud(array_points_kept)    
-    floor_a, floor_b, floor_c, floor_d = Plane.get_plane(point_cloud_nofloor)
-    
-    # Removes ??
-    list_points_nofloor = np.asarray(point_cloud_nofloor.points) 
-
-    for line in list_points_nofloor:
-        if (floor_a * line[0]) + (floor_b * line[1]) + (floor_c * line[2]) > - floor_d + diff_adj:
-            list_points_leaves.append(line)
-    
-    
-    array_points_leaves = np.asarray(list_points_leaves)
-    point_cloud_leaves = Point_cloud.array_to_point_cloud(array_points_leaves)
-
     # Save point cloud as pcd
     Point_cloud.save_as_pcd(savefilename, point_cloud_leaves)
     
-    o3d.visualization.draw_geometries([point_cloud_nofloor])
+    #o3d.visualization.draw_geometries([point_cloud_nofloor])
     o3d.visualization.draw_geometries([point_cloud_leaves])
-
-
-
     
     # Calculates execution time for the program
     end = time.time()
+    print("")
     print("Execution time:", end - start)
     
 if __name__ ==  '__main__':
