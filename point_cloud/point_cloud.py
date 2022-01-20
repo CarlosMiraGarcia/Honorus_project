20d29a24882d69e21e675e42153490f5c8d4f2e9
Carlos Mira <carlosgarciamira@gmail.com>
2022-01-20 08:53:02 +0000

    Removed break

--- point_cloud/point_cloud.py
+++ point_cloud/point_cloud.py
@@ -3,7 +3,6 @@ import numpy as np
 
 class Point_cloud:
     def remove_outliers(pcd):
-        
         voxel_down_pcd = pcd.voxel_down_sample(voxel_size=0.000001)
         cl, ind = voxel_down_pcd.remove_statistical_outlier(nb_neighbors=20,
                                                     std_ratio=2.0)
