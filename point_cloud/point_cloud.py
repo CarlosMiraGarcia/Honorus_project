6e8f387f0cab7513221c6349f3725fcc3d31df1f
Carlos Mira <carlosgarciamira@gmail.com>
2022-01-20 08:52:46 +0000

    Moved some functions to point_cloud.py, which makes more sense

--- point_cloud/point_cloud.py
+++ point_cloud/point_cloud.py
@@ -1,4 +1,5 @@
 import open3d as o3d
+import numpy as np
 
 class Point_cloud:
     def remove_outliers(pcd):
@@ -8,3 +9,19 @@ class Point_cloud:
                                                     std_ratio=2.0)
 
         return voxel_down_pcd, ind
+    
+    def array_to_point_cloud(pc_array):
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(pc_array)
+        return pcd
+    
+    def save_as_pcd(savefilename, point_cloud):
+        o3d.io.write_point_cloud(savefilename, point_cloud, write_ascii=True, compressed=True)
+        
+    def read_lines(point_cloud):    
+        list = np.asarray(point_cloud.points)
+        return list
+            
+    def append_points_to_array(npa1, npa2):
+        npa = np.vstack((npa1, npa2))
+        return npa
