630fc5e515697118a44dd0a42cc249c0588ecfd7
Carlos Mira <carlosgarciamira@gmail.com>
2022-01-20 10:41:55 +0000

    Removed break line

--- point_cloud/point_cloud.py
+++ point_cloud/point_cloud.py
@@ -6,7 +6,6 @@ class Point_cloud:
         voxel_down_pcd = pcd.voxel_down_sample(voxel_size=0.000001)
         cl, ind = voxel_down_pcd.remove_statistical_outlier(nb_neighbors=20,
                                                     std_ratio=2.0)
-
         return voxel_down_pcd, ind
     
     def array_to_point_cloud(pc_array):
