14b15c8c71748962481a80d1ae97fb2fbe94ee03
Carlos Mira <carlosgarciamira@gmail.com>
2022-01-20 08:54:09 +0000

    Moved funtions to other class that made more sense

--- stem/stem.py
+++ stem/stem.py
@@ -1,23 +1,19 @@
 import open3d as o3d
-from stem.helpers import *
 from plane.plane import *
+from point_cloud.point_cloud import *
 
 class Stem:   
-    def create_stem(point_cloud, savefilename):
-        np_array = read_lines(point_cloud)        
-        
-        Stem.floor_a, Stem.floor_b, Stem.floor_c, Stem.floor_d = Plane.get_plane(point_cloud)
+    def create_stem(point_cloud, floor_a, floor_b, floor_c, floor_d):
+        np_array = Point_cloud.read_lines(point_cloud)
                
-        xyz_plane, xyz_plane_rotated = Plane.append_plane_to_pcd(Stem.floor_a, Stem.floor_b, Stem.floor_c, Stem.floor_d)
+        xyz_plane, xyz_plane_rotated = Plane.append_plane_to_pcd(floor_a, floor_b, floor_c, floor_d)
         
-        np_array = append_points_to_list(xyz_plane, np_array)
-        np_array = append_points_to_list(xyz_plane_rotated, np_array)        
+        np_array = Point_cloud.append_points_to_array(xyz_plane, np_array)
+        np_array = Point_cloud.append_points_to_array(xyz_plane_rotated, np_array)        
       
         xyz_plane_pcd = o3d.geometry.PointCloud()
-        xyz_plane_rotated_pcd = o3d.geometry.PointCloud()
         xyz_plane_pcd.points = o3d.utility.Vector3dVector(xyz_plane)
+        xyz_plane_rotated_pcd = o3d.geometry.PointCloud()
         xyz_plane_rotated_pcd.points = o3d.utility.Vector3dVector(xyz_plane_rotated)
         
-        create_pcd(savefilename, np_array)
-        
-        return xyz_plane_rotated_pcd
+        return xyz_plane_pcd, xyz_plane_rotated_pcd
