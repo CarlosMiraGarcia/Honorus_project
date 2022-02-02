a5f68398155154936f61bc7b48d87e972a8e3643
Carlos Mira <carlosgarciamira@gmail.com>
2022-02-02 15:28:38 +0000

    Normals are now saved in the point cloud file

--- camera/camera.py
+++ camera/camera.py
@@ -1,11 +1,11 @@
 from ensenso_nxlib import api
 from camera.helpers import *
 import datetime
-
+import numpy as np
 class Camera:
     def get_point_cloud():
         camera_serial = '217617'
-        settings_path = './others/calibrations_and_patterns/latest_settings_carlos_patchmatch.json'
+        settings_path = './others/calibrations_and_patterns/parameters_28_01_22_patchmatch.json'
         path = 'test/'
         file_name = str(datetime.datetime.now().strftime('%Y%m%dT%H%M')).split('.')[0]
 
@@ -19,13 +19,17 @@ class Camera:
         rectify_raw_img()
         #recalibrate(camera_serial)
         compute_disparity()
+        path = create_path(path, file_name)
         point_cloud = create_point_map(camera_serial)
         point_cloud = reshape_point_cloud(point_cloud)
-        point_cloud = filter_nans(point_cloud)
-        path = create_path(path, file_name)
-        save_point_cloud(point_cloud, path, file_name)
+        normals = compute_normals(camera_serial)
+        normals = reshape_point_cloud(normals)
+        point_cloud_with_normals = np.concatenate([point_cloud, normals], axis= 1)
+        point_cloud_with_normals = filter_nans(point_cloud_with_normals)
+        point_cloud, normals = np.hsplit(point_cloud_with_normals, 2)
+        save_point_cloud(point_cloud, normals, point_cloud_with_normals, path, file_name)
         save_img(path, file_name, camera_serial)
-        save_ply(path, file_name, camera_serial)
+        #save_ply(path, file_name, camera_serial)
         close_camera()
         return_str = 'Images and point cloud files have been saved in the ' + path + ' folder'
         return return_str
