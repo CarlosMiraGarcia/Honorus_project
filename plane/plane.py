e159a70f75d078c3eec2384ebd75bbd253ba9e29
Carlos Mira <carlosgarciamira@gmail.com>
2022-02-03 12:31:44 +0000

    Improved the way the plane is inserted in the point cloud. Now it intersects the point cloud

--- plane/plane.py
+++ plane/plane.py
@@ -1,31 +1,45 @@
 import numpy as np
 import open3d as o3d
+from scipy.spatial.transform import Rotation
+import math
 
 class Plane:
-    def get_plane(pcd):
+    def get_plane(pcd, threshold):
         """Segments a plane in the point cloud using the RANSAC algorithm."""
-        plane_model, inliers = pcd.segment_plane(distance_threshold= 1,
+        plane_model, inliers = pcd.segment_plane(distance_threshold= threshold,
                                                  ransac_n=3,
                                                  num_iterations=1000)
         [a,b,c,d] = plane_model
         print(f"Plane equation: {a:.2f}x + {b:.2f}y + {c:.2f}z + {d:.2f} = 0")
         
-        inlier_cloud = pcd.select_by_index(inliers)
-        inlier_cloud.paint_uniform_color([1.0, 0, 0])
-        outlier_cloud = pcd.select_by_index(inliers, invert=True)
+        # inlier_cloud = pcd.select_by_index(inliers)
+        # inlier_cloud.paint_uniform_color([1.0, 0, 0])
+        # outlier_cloud = pcd.select_by_index(inliers, invert=True)
 
-        o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
+        # o3d.visualization.draw_geometries([inlier_cloud, outlier_cloud])
         return plane_model
     
-    def create_planes(a1, b1, c1, d1):
+    def create_plane(point_cloud, a1, b1, c1, d1):
         plane = []
-        rotated_plane = []
-        for i in range(0, 300):
-            for j in range(0, 100):          
-                x = -i
-                y = j
-                z = ((a1*x) + (b1*y) + d1) / -c1
-                plane.append([x, y, z])
-                rotated_plane.append([-z, y, x])
+        mean = np.median(point_cloud, axis=0)
+
         
-        return np.array(plane), np.array(rotated_plane)
+        min = np.min(point_cloud, axis=0)
+        max = np.max(point_cloud, axis=0)
+        
+        # #works        
+        # for i in range (int(min[0] * 10), int(max[0]* 10)):
+        #     for j in  range (int(min[1]* 10), int(max[1]* 10)):
+        #         x = i/10
+        #         y = j/10
+        #         z = (-d1 - a1*x - b1*y)/c1
+        #         plane.append([x, y, z])
+        
+        for i in range (int(min[0] * 10), int(max[0]* 10)):
+            for j in  range (int(min[1]* 10), int(max[1]* 10)):
+                x = i/10
+                y = j/10
+                z = (-d1 - a1*x - b1*y)/c1
+                plane.append([x, y, z])
+                
+        return np.asarray(plane)
