10b76e57410f6c169b5de2c3d8c260960a76241d
Carlos Mira <carlosgarciamira@gmail.com>
2022-02-19 09:11:14 +0000

    Added comments

--- angle_ops/angle_ops.py
+++ angle_ops/angle_ops.py
@@ -5,46 +5,55 @@ from plane_ops import plane_ops
 from point_cloud_ops import point_cloud_ops
 import statistics
 
-def calculate_angles(a1, b1, c1, a2, b2, c2):
+def _calculate_angles(a1, b1, c1, a2, b2, c2):
     """Calculates the angle between two planes:\n
-    \ta1 * x + b1 * y + c1 * z + d1 = 0 \n
+    \ta1*x + b1*y + c1*z + d1 = 0 \n
     and \n
-    \ta2 * x + b2*y + c2 * z + d2 = 0"""
+    \ta2*x + b2*y + c2*z + d2 = 0
+    """    
+    # Calculates the angle between two planes
     temp = (a1 * a2 + b1 * b2 + c1 * c2)
     e1 = math.sqrt(a1 * a1 + b1 * b1 + c1 * c1)
     e2 = math.sqrt(a2 * a2 + b2 * b2 + c2 * c2)
-    temp = temp / (e1 * e2)
-    angle = math.degrees(math.acos(temp))
-    #print(f'The angle is {angle} degrees')
-    #print("")
-    return angle
+    temp = temp / (e1 * e2)    
+    angle = math.degrees(math.acos(temp)) # Converts the angle from radians to degrees
+    
+    return angle # Returns the angle
 
 def calculate_leaf_angle(floor_plane_a, floor_plane_b, floor_plane_c, filename):
-    pc_leaf = o3d.io.read_point_cloud(filename)
-    # Calculate angles
-    print("")
-    print(f'"\033[4mCalculating angle {filename}\033[0m"')
-    line_np_points = np.asarray(pc_leaf.points)
-    line_np_normals = np.asarray(pc_leaf.normals)
-    
-    leaf_plane_a, leaf_plane_b, leaf_plane_c, leaf_plane_d =  plane_ops.get_plane(pc_leaf, 50, 1000)
-    
-    list_angle = []
-    for normal in line_np_normals:
-            list_angle.append(calculate_angles(floor_plane_a, floor_plane_b, floor_plane_c, normal[0], normal[1], normal[2]))
-    
-    
-    plane = plane_ops.create_plane(line_np_points, leaf_plane_a, leaf_plane_b, leaf_plane_c, leaf_plane_d)        
-    array_with_plane = point_cloud_ops.append_points_to_array(line_np_points, plane)
-    pc_with_plane = point_cloud_ops.array_to_point_cloud(array_with_plane)
-    point_cloud_ops.save_as_pcd(filename[:-4] + '_with_plane.pcd', pc_with_plane)    
+    """Calculates the angle between the zenith and a plane inserte into a leaf point cloud\n
+       Requires the floor plane equation (a*x + b*y + c*z+d=0) where:\n
+       \tfloor_plane_a = a,\n
+       \tfloor_plane_b = b,\n
+       \tfloor_plane_c = c,\n
+       \tand filename is the leaf point cloud file
+       """
+    pc_leaf = o3d.io.read_point_cloud(filename) # Reads point cloud from file
 
-    angle_using_normals = 270 - (statistics.median(list_angle))
-    print("Using normals", angle_using_normals)
+    leaf_np_points = np.asarray(pc_leaf.points) # Creates a numpy array with the points
+    leaf_np_normals = np.asarray(pc_leaf.normals) # Creates a numpy array with the normals    
     
-    # This need to be abs, so there is no negative angle
-    angle_using_planes = 90 + calculate_angles(floor_plane_a, floor_plane_b, floor_plane_c, leaf_plane_a, leaf_plane_b, leaf_plane_c)
+    # Gets the largest plane from the point cloud, returning the plane equation
+    leaf_plane_a, leaf_plane_b, leaf_plane_c, leaf_plane_d =  plane_ops.get_plane(pc_leaf, 50, 1000)
+        
+    # Creates a plane using the point cloud array and the plane equation    
+    plane = plane_ops.create_plane(leaf_np_points, leaf_plane_a, leaf_plane_b, leaf_plane_c, leaf_plane_d)        
+    array_with_plane = point_cloud_ops.append_points_to_array(leaf_np_points, plane) # Inserts plane to the point cloud
+    pc_with_plane = point_cloud_ops.array_to_point_cloud(array_with_plane) # Converts array to point cloud class
+    point_cloud_ops.save_as_pcd(filename[:-4] + '_with_plane.pcd', pc_with_plane) # Saves point cloud with inserted plane
+    
+    # Calculates the angles using the normals
+    list_angle = []
+    for normal in leaf_np_normals:
+        list_angle.append(_calculate_angles(floor_plane_a, floor_plane_b, floor_plane_c, normal[0], normal[1], normal[2]))
+    angle_using_normals = 270 - (statistics.median(list_angle)) # 
+    
+    # Calculates the angles using the plane equation
+    angle_using_planes = 90 + _calculate_angles(floor_plane_a, floor_plane_b, floor_plane_c, leaf_plane_a, leaf_plane_b, leaf_plane_c)
+    
+    # Since we don't know the direction of the leaf
+    # this fixes the cases where two planes don't intersect
     if angle_using_planes > 180:
         angle_using_planes = 360 - angle_using_planes
-    print("Using planes", angle_using_planes)
-    return angle_using_planes, angle_using_normals
+        
+    return angle_using_planes, angle_using_normals # Returns angles calculations
