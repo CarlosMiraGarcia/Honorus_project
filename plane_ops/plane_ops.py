1b4fc46bc28d51bdf9e0c07b1977864d35f851aa
Carlos Mira <carlosgarciamira@gmail.com>
2022-02-25 10:46:48 +0000

    Changed the foor loop to use the min and max x and y to create the plane

--- plane_ops/plane_ops.py
+++ plane_ops/plane_ops.py
@@ -1,10 +1,10 @@
 import numpy as np
-import open3d as o3d
+import open3d as o3
 
 def get_plane(pcd, threshold, iterations):
     """Segments a plane in the point cloud using the RANSAC algorithm.\n
     Finds the largest plane in a point cloud where:\n
-    \tdistance_treshold (threshold) defines the max distance from the plane to be considered an inlier,\n
+    \tdistance_threshold (threshold) defines the max distance from the plane to be considered an inlier,\n
     \transac_n defines the number of points used to consider a plane,\n
     \tand num_iterations (iterations) defines the number of times a plane is sampled
     """
@@ -23,11 +23,11 @@ def create_plane(point_cloud, a, b, c, d):
     """Creates a plane.\n
         Requires a point cloud and a plane equation a*x + b*y + c*z+d=0"""
     plane = [] # List that will contian the created plane data points       
-    min = np.min(point_cloud, axis=0) # Finds the min x value
-    max = np.max(point_cloud, axis=0) # Finds the max x value
+    min_xyz = np.min(point_cloud, axis=0) # Finds the min x, y, z value
+    max_xyz = np.max(point_cloud, axis=0) # Finds the max x, y, z value
     # Creates a plane using a plane equation
-    for i in range (int(min[0] * 10), int(max[0]* 10)):
-        for j in  range (int(min[1]* 10), int(max[1]* 10)):
+    for i in range (int(min_xyz[0] * 10), int(max_xyz[0]* 10)):
+        for j in  range (int(min_xyz[1]* 10), int(max_xyz[1]* 10)):
             x = i/10
             y = j/10
             z = (-d - a*x - b*y)/c
